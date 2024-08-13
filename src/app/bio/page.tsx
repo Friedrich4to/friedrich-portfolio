061d68a86ee9b31d279e551549166da4461baa3b
frederick <frederickros45@gmail.com>
2024-08-13 11:00:07 -0400

    fix

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,17 +1,7 @@
 import Image from "next/image";
 import { Footer } from "../components/footer";
 
-async function getTest(){
-
-  const res = await fetch('http://localhost:3000/api/collection/designer/66bad9d9761cab501b9cfd3f')
-  const data = await res.json()
-  return data.name
-}
-
 export default async function Bio() {
-
-  const test = await getTest()
-  console.log({test})
 
   return (
     <main className="flex justify-center w-screen">
@@ -26,8 +16,6 @@ export default async function Bio() {
           <p className='text-base lg:text-xl font-light'>Nacido en el 2001, viviendo actualmente en <span className="font-black">República Dominicana.</span></p>
           <p className='text-base lg:text-xl font-light'>Soy un amante del diseño en todas sus formas, ya sean objetos, experiencias, espacios, videojuegos o cualquier área en la que la planeación, intensión e ingenio tengan cabida.</p>
           <p className='text-base lg:text-xl font-light'>Me caracteriza el ser intenso y extrovertido, cualidades que se materializan en el esfuerzo de mi trabajo y en mi forma de experimentar el mundo. </p>
-
-          {test}
 
           </div>
 
